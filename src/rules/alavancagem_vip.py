ab04ab984c6d7a02d44b0db4ad85dce2080cd7e6
uesleicdoliveira@gmail.com <uesleicdoliveira@gmail.com>
2021-05-05 20:05:08 -0300

    Fix no parse do Timframe do canal Alavancagem Vip

--- src/rules/alavancagem_vip.py
+++ src/rules/alavancagem_vip.py
@@ -3,9 +3,10 @@ from src.rules.base import BaseRule
 
 class Rule(BaseRule):
     def parse_message(self, message: str) -> None:
-        pair, direction, hour, obs1, obs2 = [x.strip() for x in self.remove_emoji(message).split("\n") if x][:5]
+        pair, direction, hour, timeframe, obs1, obs2 = [x.strip() for x in self.remove_emoji(message).split("\n") if x]
         self.pair = pair
         self.hour = hour
+        self.timeframe = timeframe
         self.signal = direction
         self.obs = "%s\n%s" % (obs1, obs2)
 
--- src/settings.py
+++ src/settings.py
@@ -1,13 +1,24 @@
 import json
+from typing import Union
 
 from decouple import config
 
 from src.schema import RuleSettings
 
+
+def _normalize_log_level(level: str) -> Union[str, int]:
+    try:
+        return int(level)
+
+    except ValueError:
+        return level.upper()
+
+
 API_HASH = config("API_HASH")
 API_TOKEN = config("API_TOKEN", cast=int)
 TELEGRAM_BOT_API_KEY = config("TELEGRAM_BOT_API_KEY")
 TELEGRAM_STRING_SESSION = config("TELEGRAM_STRING_SESSION")
+LOG_LEVEL = config("LOG_LEVEL", default="info", cast=_normalize_log_level)
 
 with open("settings.json", "r", encoding="utf-8") as f:
     RULE_SETTINGS = json.load(f)
